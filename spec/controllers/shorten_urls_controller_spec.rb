bd55c819af4808852b865d16b1eed05e2f6ece50
ali <likesteel@gmail.com>
2017-10-16 22:16:26 +0500

    added a new test

--- spec/controllers/shorten_urls_controller_spec.rb
+++ spec/controllers/shorten_urls_controller_spec.rb
@@ -25,6 +25,15 @@ RSpec.describe ShortenUrlsController, type: :controller do
       }.to change(ShortenUrl, :count).by(1)
     end
 
+    it "should increase the count if url is valid and with params" do
+      @shorten_url_params = { url: 'http://www.amazon.com/Kindle-Wireless-Reading-Display-Globally/dp/
+       B003FSUDM4/
+       ref=amb_link_353259562_2?pf_rd_m=ATVPDKIK X0DER&pf_rd_s=center-10&pf_rd_r=11EYKTN682A79T370AM3&pf_rd_t=201&pf_rd_p=1270985982&pf_rd_i=B002Y27P3M ' }
+      expect {
+        post :create, params: { shorten_url: @shorten_url_params }
+      }.to change(ShortenUrl, :count).by(1)
+    end
+
     it "should render shorten js template" do
       @shorten_url_params = { url: 'http://www.test.com' }
       post :create, params: { shorten_url: @shorten_url_params }, :format => "js"
